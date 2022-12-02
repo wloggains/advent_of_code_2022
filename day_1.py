cefe7419541c5a26fa30f01b6a7fc08928b016a5
wloggains <wloggains@nu.edu>
2022-12-01 18:15:15 -0600

    Adding solution.

--- day_1.py
+++ day_1.py
@@ -1,3 +1,24 @@
 
+# Get input
+input_file = 'day_1_input.txt'
+with open(input_file) as f:
+    lines = f.read().splitlines()
 
-print("test")
+# Initialize
+current_elf = 0
+elves = []
+
+# Parse each line
+for line in lines:
+    
+    if line:
+        current_elf += int(line)
+
+    else:
+        elves.append(current_elf)
+        current_elf = 0
+    
+elves.sort()
+print(f"The top elf is holding {elves[-1]}.")
+
+print(f"The top three are holding {sum(elves[-3:])}.")
